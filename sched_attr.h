572d06271e9c5e20a7c30805dd35a8d1ceb3bdec
Elliott Hughes <enh@google.com>
2023-06-28 15:23:17 +0000

    Don't redefine `struct sched_attr`.
    We're going to expose this from bionic's <sched.h> specifically to stop
    people copy & pasting this struct everywhere.
    
    Unfortunately, the kernel __u64 type doesn't match our PRIu64 macro, so
    we need to redo the 32- and 64-bit printf() stuff here. While I'm here,
    since the only reason anyone ever talks about sched_attr is for the
    newer uclamp stuff, I've added decoding of the two extra fields in the
    newer version of the struct (using the two size constants that we get
    for free from the uapi headers).
    
    Bug: http://b/289090109
    Test: treehugger
    Change-Id: I4a7d863ca34d07367a88bd0380f17ddf3ef0aa17

--- sched_attr.h
+++ sched_attr.h
@@ -3,17 +3,6 @@
 
 # include <stdint.h>
 
-struct sched_attr {
-	uint32_t size;
-	uint32_t sched_policy;
-	uint64_t sched_flags;
-	uint32_t sched_nice;
-	uint32_t sched_priority;
-	uint64_t sched_runtime;
-	uint64_t sched_deadline;
-	uint64_t sched_period;
-};
-
 # define SCHED_ATTR_MIN_SIZE	48
 
 #endif /* !STRACE_SCHED_ATTR_H */
